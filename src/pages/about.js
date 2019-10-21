2826d10e0f7be57653dc08e630c15be9b25c3789
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2019-10-21 13:45:11 -0400

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => (
+  <Layout>
+    <SEO title="About" />
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+        voluptatibus qui dolores dicta magni impedit? Amet inventore
+        necessitatibus delectus eaque ipsum. Veniam quae quidem accusantium
+        cupiditate. Sapiente culpa ducimus ipsa!
+      </p>
+    </div>
+  </Layout>
+)
+
+export default About
